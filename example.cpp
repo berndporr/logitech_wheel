ec67cdbe5efc7d89b1463825cdad33b7f0277753
Bernd Porr <mail@berndporr.me.uk>
2025-08-04 16:07:39 +0100

    Doesn't work at the moment

--- example.cpp
+++ example.cpp
@@ -1,17 +1,7 @@
+#include "logiwheel.h"
 
-
-int main(int argc, char *argv[])
-    {
-    const char *device;
-    int js;
-    struct js_event event;
-    struct axis_state axes[3] = {0};
-    size_t axis;
-
-    if (argc > 1)
-        device = argv[1];
-    else
-        device = "/dev/input/js0";
-
-    return 0;
+int main(int argc, char *argv[]) {
+	LogiWheel logiwheel;
+	logiwheel.start();
+	return 0;
 }
